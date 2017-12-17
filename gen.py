701f8770a84b805c05cea72d1c88af3a3a8f04d6
esquivel71 <32263317+esquivel71@users.noreply.github.com>
2017-12-17 17:50:50 +0000

    cenas e tal

--- gen.py
+++ gen.py
@@ -1,5 +1,11 @@
 import random
+import sys
 
-for i in range(0, 11):
-	x = random.randint(7500, 12500)
-	print("%d" % (x))
+if (len(sys.argv)==2):
+	for i in range(0, int(sys.argv[1])):
+		x = random.randint(7500, 12500)
+		print("%d" % (x))
+else:
+	for i in range(0, 11):
+		x = random.randint(7500, 12500)
+		print("%d" % (x))
